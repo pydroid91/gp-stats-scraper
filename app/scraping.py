ec17287c772087c360d95fb974661ca9e16fe513
pydroid91 <temich-ivanoff@yandex.ru>
2024-12-24 00:15:00 +0500

    qualifying stats were removed; saving a plot to a binary stream and getting soup object are separate functions

--- app/scraping.py
+++ app/scraping.py
@@ -3,7 +3,6 @@ matplotlib.use("Agg")
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
-import pprint
 import pandas as pd
 from itertools import accumulate
 import io
@@ -11,11 +10,42 @@ import io
 HEADER = {'User-Agent': 'Mozilla/5.0'}
 
 
+def save_plot():
+    b = io.BytesIO()
+    plt.savefig(b, format="png")
+    b.seek(0)
+    plt.close()
+    return b
+
+
+def save_empty_cc_plot():
+    plt.figure()
+    plt.plot()
+    plt.text(0, 0, "Кубок конструкторов впервые начал разыгрываться в 1958г.", ha="center", va="center")
+    plt.axis("off")
+    b = io.BytesIO()
+    plt.savefig(b, format="png")
+    b.seek(0)
+    plt.close()
+    return b
+
+
+def save_best_laps_plots(info_dict):
+    plt.figure()
+    plt.bar(info_dict.keys(), info_dict.values())
+    plt.xticks(fontsize=8)
+    b = save_plot()
+    return b
+
+
+def get_soup(url):
+    src = requests.get(url, headers=HEADER).text
+    return BeautifulSoup(src, "lxml")
+
+
 # creates bar plots of number of best laps for drivers and constructors in certain season
 def get_best_laps(year):
-    bl_url = f"https://gpracingstats.com/seasons/{year}-world-championship/fastest-laps/"
-    bl_src = requests.get(bl_url, headers=HEADER).text
-    bl_soup = BeautifulSoup(bl_src, "lxml")
+    bl_soup = get_soup(f"https://gpracingstats.com/seasons/{year}-world-championship/fastest-laps/")
 
     bl_drivers = {}
     bl_constructors = {}
@@ -38,39 +68,15 @@ def get_best_laps(year):
         else:
             bl_constructors[constructor] += 1
 
-    plt.figure()
-    plt.bar(bl_constructors.keys(), bl_constructors.values())
-    plt.xticks(fontsize=8)
-    b = io.BytesIO()
-    plt.savefig(b, format="png")
-    b.seek(0)
-    plt.close()
-
-    plt.figure()
-    plt.bar(bl_drivers.keys(), bl_drivers.values())
-    b2 = io.BytesIO()
-    plt.savefig(b2, format="png")
-    b2.seek(0)
-    plt.close()
-    return [b, b2]
+    return [save_best_laps_plots(bl_constructors), save_best_laps_plots(bl_drivers)]
 
 
 # creates pie chart of constructors cup points in certain season
 def get_constructors_cup_points(year):
     if year < 1958:
-        plt.figure()
-        plt.plot()
-        plt.text(0, 0, "Кубок конструкторов впервые начал разыгрываться в 1958г.", ha="center", va="center")
-        plt.axis("off")
-        b = io.BytesIO()
-        plt.savefig(b, format="png")
-        b.seek(0)
-        plt.close()
-        return b
+        return save_empty_cc_plot()
 
-    pts_url = f"https://gpracingstats.com/seasons/{year}-world-championship/constructor-standings/"
-    pts_src = requests.get(pts_url, headers=HEADER).text
-    pts_soup = BeautifulSoup(pts_src, "lxml")
+    pts_soup = get_soup(f"https://gpracingstats.com/seasons/{year}-world-championship/constructor-standings/")
 
     points = {}
     for row in pts_soup.find(class_="summary season-standings constructor").find("tbody").find_all("tr")[:-1]:
@@ -92,18 +98,12 @@ def get_constructors_cup_points(year):
 
     plt.figure()
     plt.pie(points.values(), labels=points.keys())
-    b = io.BytesIO()
-    plt.savefig(b, format="png")
-    b.seek(0)
-    plt.close()
-    return b
+    return save_plot()
 
 
 # returns point system used in certain season
 def get_point_system(year):
-    champ_url = f"https://gpracingstats.com/seasons/{year}-world-championship/driver-standings/"
-    champ_src = requests.get(champ_url, headers=HEADER).text
-    champ_soup = BeautifulSoup(champ_src, "lxml")
+    champ_soup = get_soup(f"https://gpracingstats.com/seasons/{year}-world-championship/driver-standings/")
 
     points = [0]
     fastest_lap_point = 0
@@ -120,10 +120,7 @@ def replace_positions_with_points(results, year):
     points, fl_points = get_point_system(year)
 
     for i in range(len(results)):
-        if "float" in str(type(results[i])):
-            results[i] = 0
-            continue
-        if not results[i].isnumeric():
+        if "float" in str(type(results[i])) or not results[i].isnumeric():
             results[i] = 0
             continue
         if "(" in results[i]:
@@ -148,26 +145,16 @@ def get_championships_graph(year):
         driver_record = table["Driver"].iloc[i]
         driver_name = driver_record[driver_record.find(")") + 1: driver_record.find("(", 2)]
         plt.plot(labels, y, label=driver_name)
+
     plt.legend(loc="upper left")
     plt.xticks(rotation=90)
-    b = io.BytesIO()
-    plt.savefig(b, format="png")
-    b.seek(0)
-    plt.close()
-    return b
+    return save_plot()
 
 
 def get_constructors_cup_graph(year):
     if year < 1958:
-        plt.figure()
-        plt.plot()
-        plt.text(0, 0, "Кубок конструкторов впервые начал разыгрываться в 1958г.", ha="center", va="center")
-        plt.axis("off")
-        b = io.BytesIO()
-        plt.savefig(b, format="png")
-        b.seek(0)
-        plt.close()
-        return b
+        return save_empty_cc_plot()
+
     champ_url = f"https://gpracingstats.com/seasons/{year}-world-championship/constructor-standings/"
     table = pd.read_html(champ_url, index_col=0)[0]
     labels = list(table.columns)[1:-1]
@@ -193,35 +180,7 @@ def get_constructors_cup_graph(year):
     plt.legend(loc="upper left")
     plt.xticks(rotation=90)
 
-    b = io.BytesIO()
-    plt.savefig(b, format="png")
-    b.seek(0)
-    plt.close()
-    return b
-
-
-def get_h2h_qualifying_score(year):
-    qualy_url = f"https://gpracingstats.com/seasons/{year}-world-championship/qualifying-stats/"
-    qualy_src = requests.get(qualy_url, headers=HEADER).text
-    qualy_soup = BeautifulSoup(qualy_src, "lxml")
-
-    teams = {}
-    drivers1 = []
-    drivers2 = []
-
-    for row in qualy_soup.find(class_="summary quali-h2h sortable").find("tbody").find_all("tr"):
-        data = row.find_all("td")
-        constructor = data[0].text
-        drivers1.append(data[1].text)
-        drivers2.append(data[3].text)
-        score1, score2 = map(int, data[2].text.split("-"))
-
-        if constructor not in teams:
-            teams[constructor] = [score1, score2]
-    df = pd.DataFrame.from_dict(teams, orient="index", columns=["D1", "D2"])
-    df.plot.bar()
-
-    plt.show()
+    return save_plot()
 
 
 def parse_all_plots(year):
